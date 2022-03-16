cb30fb0b661844fbeba1e1912a67bcc8bc1fef9a
Dalvin <dalvin.josias@gmail.com>
2022-03-16 10:44:25 -0700

    init moralis

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { MoralisProvider } from "react-moralis";
-import {
-  ChakraProvider,
-  extendTheme
-} from '@chakra-ui/react';
-import Home from "./Home";
+import { MoralisProvider } from 'react-moralis';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import Home from './Home';
 
 const theme = extendTheme({
   config: {
-    initialColorMode: 'dark'
-  }
-})
+    initialColorMode: 'dark',
+  },
+});
+
+const appId = "dFjUQiVswnhAiO4NdpyFWsRqL850DlhTNc4POy9b"
+const serverUrl = "https://qoh9qcizymqr.usemoralis.com:2053/server"
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Home/>
-    </ChakraProvider>
+    <MoralisProvider appId={appId} serverUrl={serverUrl}>
+      <ChakraProvider theme={theme}>
+        <Home />
+      </ChakraProvider>
+    </MoralisProvider>
   );
 }
 

a2e45d38e50b61a518e81d97e017e904dc07f88d
Dalvin <dalvin.josias@gmail.com>
2022-03-16 23:05:36 -0700

    setup auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import Home from './Home';
-
+import {
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Grid
+} from '@chakra-ui/react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Box
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '10px',
+        }}
+      >
+        <Grid p={3}>
+          <Breadcrumb separator="|">
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">Home</BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
+          <ColorModeSwitcher justifySelf="flex-end" />
+        </Grid>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Box>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,33 @@
 import {
   Box,
-  Text,
-  Link,
   VStack,
-  Code,
   Grid,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
+  Container,
+  Heading
+} from '@chakra-ui/layout';
+
+import { useMoralis } from "react-moralis";
 
 function Home() {
+  const {authenticate, isAuthenticated, logout} = useMoralis()
+
+  if(isAuthenticated) {
+    return(
+      <>
+        <Container>
+          <Heading>Welcome</Heading>
+        </Container>
+      </>
+    );
+  }
+  
+
+
   return (
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Text>
-            Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          <Heading>Login</Heading>
         </VStack>
       </Grid>
     </Box>

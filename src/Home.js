c433c3c29a67fc55436e134a7164c5ac10555e21
Dalvin <dalvin.josias@gmail.com>
2022-03-19 14:30:11 -0700

    fix auth

--- src/Home.js
+++ src/Home.js
@@ -1,52 +1,21 @@
-import {
-  Box,
-  VStack,
-  Grid,
-  Container,
-  Heading
-} from '@chakra-ui/layout';
+import { Container, Heading } from '@chakra-ui/layout';
 
-import {
-  AlertTitle,
-  AlertDescription,
-  Alert,
-  AlertIcon,
-  Button
-} from '@chakra-ui/react';
+import Auth from './Auth';
 
-import { useMoralis } from "react-moralis";
+import { useMoralis } from 'react-moralis';
 
 function Home() {
-  const {authenticate, isAuthenticated, logout, authError} = useMoralis()
+  const { isAuthenticated } = useMoralis();
 
-  if(isAuthenticated) {
-    return(
-      <Heading>Test</Heading>
-    )
+  if (isAuthenticated) {
+    return <Heading>Test</Heading>;
   }
-  
 
-
-  return(
+  return (
     <>
       <Container>
-        <Box>
-          {
-            authError && (
-              <Alert status="error">
-                <AlertIcon />
-                <Box flex="1">
-                  <AlertTitle>Authentication has failed</AlertTitle>
-                  <AlertDescription display="block">
-                    {authError.message}
-                  </AlertDescription>
-                </Box>
-              </Alert>
-            )
-          }
-        </Box>
-        <Heading>Welcome</Heading>
-        <Button onClick={() => authenticate()} isLoading={isAuthenticated}>Connect</Button>
+        <Heading>Reality Dapp</Heading>
+        <Auth />
       </Container>
     </>
   );

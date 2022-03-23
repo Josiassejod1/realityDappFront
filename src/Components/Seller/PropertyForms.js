e77b45638ff45724354d778c28a4452815cafbac
Dalvin <dalvin.josias@gmail.com>
2022-03-23 11:49:51 -0700

    form validation

--- src/Components/Seller/PropertyForms.js
+++ src/Components/Seller/PropertyForms.js
@@ -13,6 +13,7 @@ import {STATES} from '../../const';
 
 export function AddressForm() {
   const [address, setAddress] = useState();
+  const [address2, setAddress2] = useState();
   const [city, setCity] = useState();
   const [state, setState] = useState();
   const [zip, setZip] = useState();
@@ -21,24 +22,29 @@ export function AddressForm() {
     <Container>
       <FormControl isRequired>
         <FormLabel htmlFor="address1">Address 1</FormLabel>
-        <Input id="address1" placeholder="Address" />
+        <Input id="address1" placeholder="Address" onChange={(e) => setAddress(e.target.value)}/>
       </FormControl>
       <FormControl>
         <FormLabel htmlFor="address2">Address 2</FormLabel>
-        <Input id="address2" placeholder="Address" />
+        <Input id="address2" placeholder="Address" onChange={(e) => setAddress2(e.target.value)}/>
       </FormControl>
       <FormControl isRequired>
         <FormLabel htmlFor="city">City</FormLabel>
-        <Input id="city" placeholder="City" />
+        <Input id="city" placeholder="City" onChange={(e) => setCity(e.target.value)}/>
       </FormControl>
       <FormControl>
         <FormLabel htmlFor="state">State</FormLabel>
-        <Select id="state" placeholder="State">
+        <Select id="state" placeholder="State" onChange={(e) => setState(e.target.value)}>
           {STATES.map(state => {
             return <option>{state}</option>;
           })}
         </Select>
       </FormControl>
+      <FormControl isRequired>
+        <FormLabel htmlFor="zip-code">City</FormLabel>
+        <Input id="zip-code" placeholder="Zip Code" onChange={(e) => setZip(e.target.value)}/>
+      </FormControl>
+      <FormControl></FormControl>
     </Container>
   );
 }

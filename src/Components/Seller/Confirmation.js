7e7571916967f34baece25989ed44193a51b3ec4
Dalvin <dalvin.josias@gmail.com>
2022-04-14 16:30:23 -0700

    add info

--- src/Components/Seller/Confirmation.js
+++ src/Components/Seller/Confirmation.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Button, Container, Grid, List, Text, Image, ListItem } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  Grid,
+  List,
+  Text,
+  Image,
+  ListItem,
+} from '@chakra-ui/react';
 
 function Confirmation({ prevStep, nextStep, values }) {
   console.log(values);
@@ -32,37 +40,51 @@ function Confirmation({ prevStep, nextStep, values }) {
       <div>
         <List>
           <ListItem>
+            <FormLabel htmlFor="address1">Address 1</FormLabel>
             <Text>{address1}</Text>
           </ListItem>
           <ListItem>
+            <FormLabel htmlFor="address2">Address 2</FormLabel>
             <Text>{address2}</Text>
           </ListItem>
           <ListItem>
+            <FormLabel htmlFor="city">City</FormLabel>
             <Text>{city}</Text>
           </ListItem>
           <ListItem>
+            <FormLabel htmlFor="state">State</FormLabel>
             <Text>{state}</Text>
           </ListItem>
           <ListItem>
+            <FormLabel htmlFor="zipCode">Zip Code</FormLabel>
             <Text>{zipCode}</Text>
           </ListItem>
           <ListItem>
+            <FormLabel htmlFor="baths">Baths</FormLabel>
             <Text>{baths}</Text>
           </ListItem>
           <ListItem>
+            <FormLabel htmlFor="beds">Beds</FormLabel>
             <Text>{beds}</Text>
           </ListItem>
           <ListItem>
+            <FormLabel htmlFor="sqrft">Square Feet</FormLabel>
             <Text>{squareFeet}</Text>
           </ListItem>
           <ListItem>
+            <FormLabel htmlFor="price">Price</FormLabel>
             <Text>{price}</Text>
           </ListItem>
           <ListItem>
+            <FormLabel htmlFor="price">Description</FormLabel>
             <Text>{description}</Text>
           </ListItem>
           <ListItem>
-            <Image src={window.URL.createObjectURL(images)} height="250" width="250" />
+            <Image
+              src={window.URL.createObjectURL(images)}
+              height="250"
+              width="250"
+            />
           </ListItem>
           <ListItem>
             <Image src={documents} height="250" width="250" />
--- src/Components/Seller/UploadImage.js
+++ src/Components/Seller/UploadImage.js
@@ -1,71 +1,82 @@
-import {
-    Button,
-    FormLabel,
-    Flex,
-    Image
-  } from '@chakra-ui/react';
+import { Button, FormLabel, Flex, Image, Center } from '@chakra-ui/react';
 
-  import React, {useState} from "react";
+import React, { useState } from 'react';
 function UploadImage({ setProperties, values, nextStep, prevStep }) {
-    const [selectedFile, setSelectedFile] = useState(null);
-    const Continue = e => {
-      e.preventDefault();
-      console.log(values);
-      nextStep();
-    };
-    
-    console.log(setProperties)
-  
-    const Previous = e => {
-      console.log(values);
-      e.preventDefault();
-      prevStep();
-    };
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedDeed, setSelectedDeed] = useState(null);
+  const Continue = e => {
+    e.preventDefault();
+    console.log(values);
+    nextStep();
+  };
 
-    const fileData = () => {
-        if (selectedFile) {
-            return (
-                <div>
-                    {/* <h2>File Details:</h2>
+  console.log(setProperties);
 
-                    <p>File Name: {selectedFile.name}</p>
+  const Previous = e => {
+    console.log(values);
+    e.preventDefault();
+    prevStep();
+  };
 
+  const fileData = () => {
+    if (selectedFile) {
+      return (
+        <div>
+          <Center>
+            <Image src={window.URL.createObjectURL(selectedFile)} />
+          </Center>
+        </div>
+      );
+    }
+  };
 
-                    <p>File Type: {selectedFile.type}</p>
+  const fileDeed = () => {
+    if (selectedDeed) {
+      return (
+        <div>
+          <Center>
+            <Image src={window.URL.createObjectURL(selectedDeed)} />
+          </Center>
+        </div>
+      );
+    }
+  };
 
-
-                    <p>
-                        Last Modified:{" "}
-                        {selectedFile.lastModifiedDate.toDateString()}
-                    </p> */}
-                    <Image src={window.URL.createObjectURL(selectedFile)} height="250" width="250" />
-                </div>
-            );
-        }
-    };
-  
-    return(
-        <>
-        <FormLabel htmlFor="images">Upload Images</FormLabel>
-        <input
-          required
-          type="file"
-          onChange={e => {
-            setProperties(prevProperty => ({
-                ...prevProperty,
-                "images": e.target.files[0],
-              }))
-              setSelectedFile(e.target.files[0])
-          }}
-        />
-        <Flex p={2}>
-          <Button onClick={Previous}>Previous</Button>
-          <Button onClick={Continue}>Continue</Button>
-        </Flex>
-        {fileData()}
-      </>
-    )
-  }
-  
+  return (
+    <>
+      <FormLabel htmlFor="images">Upload Property Image</FormLabel>
+      <input
+        required
+        type="file"
+        onChange={e => {
+          setProperties(prevProperty => ({
+            ...prevProperty,
+            images: e.target.files[0],
+          }));
+          setSelectedFile(e.target.files[0]);
+        }}
+      />
+      <Flex p={2}>
+        <Button onClick={Previous}>Previous</Button>
+        <Button onClick={Continue}>Continue</Button>
+      </Flex>
+      {fileData()}
+        <br/>
+      <FormLabel htmlFor="deed">Upload Property Deed</FormLabel>
+      <input
+        required
+        type="file"
+        onChange={e => {
+          setProperties(prevProperty => ({
+            ...prevProperty,
+            document: e.target.files[0],
+          }));
+          setSelectedDeed(e.target.files[0]);
+        }}
+      />
+      {fileDeed()}
+    </>
+  );
+}
 
 export default UploadImage;

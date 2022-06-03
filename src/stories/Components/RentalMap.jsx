cb61f80653f8f2f504f3f99ff2cf96c854e269bf
Dalvin <dalvin.josias@gmail.com>
2022-06-02 23:32:02 -0700

    setup basic mp

--- src/stories/Components/RentalMap.jsx
+++ src/stories/Components/RentalMap.jsx
@@ -1,12 +1,11 @@
-import { Box } from '@chakra-ui/react';
 import { Map, ZoomControl } from 'pigeon-maps';
-import { stamenToner } from 'pigeon-maps/providers';
-
+import { osm } from 'pigeon-maps/providers'
 export const RentalMap = ({ markers }) => {
   return (
     <Map
       height={937}
       width={598}
+      provider={osm}
       defaultCenter={[37.7749, 122.4194]}
       defaultZoom={11}
     >
